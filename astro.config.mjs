a1b63991af10350826d3acfa2599d6670d138d79
ansetechnoapp <129697695+ansetechnoapp@users.noreply.github.com>
2024-05-09 14:12:14 +0100

    portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://anscod.online/portfolio/',
-    root: 'foo',
+    // root: 'foo',
     // base: './portfolio/',
+    publicDir: 'portfolio',
 });
