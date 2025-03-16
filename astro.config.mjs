4884cff3db2a0aa36c119930f713c1db80fd7a73
otty kevin <anselmetechnoapp@gmail.com>
2025-03-16 09:09:10 +0100

    chore: comment out react import in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
-import react from '@astrojs/react';
+// import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
